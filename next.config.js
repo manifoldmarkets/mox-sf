ff9a87efe19e9ac9644e0f7b0489e344c844a8ae
Austin Chen <akrolsmir@gmail.com>
2025-02-15 10:35:30 -0800

    Reroute /apply to signup form

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/apply",
+        destination:
+          "https://airtable.com/appkHZ2UvU6SouT5y/pagGp5iw06B9Fc57g/form",
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
