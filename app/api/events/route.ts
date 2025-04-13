a0e330dc8b253f8eea0b0e8e060f52f0f183f0df
Austin Chen <akrolsmir@gmail.com>
2025-04-12 18:10:45 -0700

    List this week + upcoming events

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -3,7 +3,8 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   try {
     const res = await fetch(
-      'https://api.airtable.com/v0/appkHZ2UvU6SouT5y/Events?maxRecords=100&view=Tentative%20upcoming',
+      // This view includes upcoming events, plus events in last 7 days
+      'https://api.airtable.com/v0/appkHZ2UvU6SouT5y/Events?maxRecords=100&view=viwSk5Z39fSwtPGaB',
       {
         headers: {
           Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
